6176822170c17de11867ae10065e88f031557d2e
siddiqui1034 <nausheensiddiqui11@gmail.com>
2024-09-01 21:04:53 +0530

    01-09-2024

--- routes/Controllers/customerController.js
+++ routes/Controllers/customerController.js
@@ -1,130 +1,134 @@
-
 var express = require('express')
 const {
-    regService, 
-    loginService, 
+    regService,
+    addressListService,
+    saveAddressService,
+    loginService,
     getProductsService,
-    getProductByIdService, 
+    getProductByIdService,
+    updateProfileService,
     saveOrderService,
+    getOrdersService,
     cancelOrderService,
     getCartService,
     saveToCartService,
-    deleteCartService } = require('../Services/customerServices')
+    deleteCartService,
+    getCustomerByIdService
+} = require('../Services/customerServices')
 var router = express.Router()
 const jwt = require('jsonwebtoken')
 const validateToken = require('../../utils/validateToken')
 
-router.post("/login", 
-    async function(req, res, next){
-    try {
-        const result = await loginService(req)
-        res.send(result);
-    }
-    catch (exception) {
-        console.error(exception)
-        res.send(exception.message)
-    }
-})
+router.post("/login",
+    async function (req, res, next) {
+        try {
+            const result = await loginService(req)
+            res.send(result);
+        }
+        catch (exception) {
+            console.error(exception)
+            res.send(exception.message)
+        }
+    })
 
-router.post("/register", 
+router.post("/register",
     async (req, res, next) => {
-    try {
-        const result = await regService(req)
-        // you can not directly return with return keyword to the client so we will use res
-        res.send(result)
-    }
-    catch (exception) {
-        console.log(exception)
-        res.send(`customer controller error ${exception.message}`)
-    }
-})
+        try {
+            const result = await regService(req)
+            // you can not directly return with return keyword to the client so we will use res
+            res.send(result)
+        }
+        catch (exception) {
+            console.log(exception)
+            res.send(`customer controller error ${exception.message}`)
+        }
+    })
+
+router.post("/saveOrder",
+    validateToken,
+    function (req, res, next) {
+        try {
+            (async function () {
+                const result = await saveOrderService(req)
+                res.send(result)
+            })()
+        }
+        catch (ex) {
+            console.error(ex);
+            res.send(ex.message)
+        }
+    })
 
 router.get("/ordersList",
     validateToken,
     function (req, res, next) {
-        try{
+        try {
             (async function () {
                 const result = await getOrdersService(req)
+                // console.log("OrderListController", result)
                 res.send(result);
             }
             )()
-        }catch(ex){
-            res.send(ex.message)
-        }       
-    }
-)
-
-router.post("/saveOrder",
-    validateToken,
-     function(req, res, next){
-        try{
-            (async function() {
-                const result = await saveOrderService()
-                res.send(result)
-            })()
-        }
-        catch(ex){
+        } catch (ex) {
             console.error(ex);
             res.send(ex.message)
         }
-
-})
+    }
+)
 
 router.delete("/cancelOrder",
     validateToken,
-    function (req, res, next){
-        try{
-            (async ()=>{
+    function (req, res, next) {
+        try {
+            (async () => {
                 const result = await cancelOrderService(req)
                 res.send(result);
             })()
-        }catch(ex){
+        } catch (ex) {
             console.error(ex);
             res.send(ex.message)
         }
-    }
-)
-
-router.get("/cartList",
-    validateToken,
-    function (req, res, next) {
-        try{
-            (async function () {
-                const result = await getCartService(req)
-                res.send(result);
-            }
-            )()
-        }catch(ex){
-            res.send(ex.message)
-        }       
     }
 )
 
 router.post("/saveToCart",
     validateToken,
-     function(req, res, next){
-        try{
-            (async function() {
+    function (req, res, next) {
+        try {
+            (async function () {
                 const result = await saveToCartService(req)
                 res.send(result)
             })()
         }
-        catch(ex){
+        catch (ex) {
             console.error(ex);
             res.send(ex.message)
         }
+    })
 
-})
+router.get("/cartList",
+    validateToken,
+    function (req, res, next) {
+        try {
+            (async function () {
+                const result = await getCartService(req)
+                res.send(result);
+            })()
+        } catch (ex) {
+            res.send(ex.message)
+        }
+    }
+)
 
 router.delete("/deleteCart",
     validateToken,
-    function (req, res, next){
-        try{
-            (async ()=>{
+    function (req, res, next) {
+        try {
+            (async () => {
                 const result = await deleteCartService(req)
                 res.send(result);
             })()
-        }catch(ex){
+        } catch (ex) {
             console.error(ex);
             res.send(ex.message)
         }
@@ -146,18 +150,60 @@ router.get("/getProducts",
 
     })
 
-router.get("/getProductById", function(req, res, next){
-    try{
-        (async ()=>{
-            const result = await getProductByIdService(req)
+router.get("/getCustomerById", function (req, res, next) {
+    try {
+        (async () => {
+            const result = await getCustomerByIdService(req)
             res.send(result);
         })()
-    }catch(ex){
+    } catch (ex) {
         console.error(ex)
         res.send(ex.message);
     }
 })
-     
+
+router.post('/saveAddress', validateToken, function (req, res, next) {
+    try {
+        (
+            async function () {
+                const result = await saveAddressService(req);
+                res.send(result);
+            }
+        )()
+    } catch (ex) {
+        console.error(ex);
+        res.send(ex.message);
+    }
+})
+
+router.get('/addressList', validateToken, function (req, res, next) {
+    try {
+        (async function () {
+            const result = await addressListService(req)
+            res.send(result);
+        }
+        )()
+    } catch (ex) {
+        console.error(ex.message);
+    }
+}
+)
+
+// updating user Profile
+router.put("/updateProfile",
+    validateToken,
+    function (req, res, next) {
+        try {
+            (async () => {
+                const result = await updateProfileService(req)
+                res.send(result);
+            })()
+        } catch (ex) {
+            console.error(ex)
+            res.send(ex.message);
+        }
+    })
+
 module.exports = router;
 
 /*
--- routes/dao/customerDAO.js
+++ routes/dao/customerDAO.js
@@ -5,87 +5,194 @@ const { get } = require('../Controllers/customerController');
 // const getDatabase = require ('../../utils/getDBConnection')
 
 
-async function loginDAO(data){
-console.log("LoginDAO");
-const dbObject = await getDB()
-const dbCollectionObject = dbObject.collection("customers")
-const resultLogin = await dbCollectionObject.findOne(data)
-return resultLogin;
+async function loginDAO(data) {
+    // console.log("LoginDAO");
+    const dbObject = await getDB()
+    const dbCollectionObject = dbObject.collection("customers")
+    const resultLogin = await dbCollectionObject.findOne(data)
+    if (resultLogin) {
+        const cart = dbObject.collection("cart")
+        const count = await cart.countDocuments({ uid: resultLogin?._id?.toString() })
+        resultLogin.count = count;
+    }
+    console.log(1122, resultLogin)
+    return resultLogin;
 }
 
-async function regDAO(data){
- console.log("DAO");
- const dbObject = await getDB()  //getDatabase returns a promise so through await we can retrieve promise data
- const collectionObject = dbObject.collection("customers")
- const result = await collectionObject.insertOne(data)  
- return result;
-}
-
-async function getOrdersDAO(id){
-    const db = await getDB()
-    const collection = db.collection("orders")
-    const result = await collection.find({_id: ObjectId.createFromHexString(id)}).toArray();
+async function regDAO(data) {
+    console.log("DAO");
+    const dbObject = await getDB()  //getDatabase returns a promise so through await we can retrieve promise data
+    const collectionObject = dbObject.collection("customers")
+    const result = await collectionObject.insertOne(data)
     return result;
 }
 
-async function getProductsDAO(){
+// async function getOrdersDAO(id){
+//     const db = await getDB()
+//     const collection = db.collection("orders")
+//     const result = await collection.find({ id }).toArray();
+//     return result;
+// }
+
+async function getOrdersDAO(id) {
+    const db = await getDB()
+    const orders = db.collection("orders");
+    // const products = db.collection("products");
+
+    const result = await orders.aggregate([
+        {
+            $lookup: {
+                from: "products",
+                localField: 'productId',
+                foreignField: '_id',
+                as: 'productDetails'
+            }
+        },
+        {
+            $unwind: '$productDetails'
+        },
+        {
+            $project: {
+                _id: 1,
+                productId: 1,
+                productDetails: 1
+            }
+        }
+    ]).toArray();
+    console.log(1000, result)
+    return result;
+}
+
+async function saveOrderDAO(data) {
+    const db = await getDB()
+    // const productId = ObjectId.createFromHexString(data.productId)
+    const collection = db.collection("orders")
+    const result = await collection.insertOne({ ...data, productId: ObjectId.createFromHexString(data.productId) });
+    return result;
+}
+
+async function cancelOrderDAO(orderId) {
+    const db = await getDB()
+    const collection = db.collection("orders")
+    const result = await collection.updateOne({ _id: ObjectId.createFromHexString(orderId) }, { $set: { status: 'cancel' } })
+    return result;
+}
+
+async function getProductsDAO() {
     const db = await getDB()
     const collection = db.collection("products")
     const result = await collection.find({}).toArray();
     return result;
 }
 
-async function getProductByIdDAO(id){
+async function getProductByIdDAO(id) {
     const db = await getDB()
     const collection = db.collection("products")
-    const result = await collection.find({_id: ObjectId.createFromHexString(id)}).toArray();
+    const result = await collection.find({ _id: ObjectId.createFromHexString(id) }).toArray();
     return result[0];
 }
 
-async function saveOrderDAO(data){
+// async function getCartDAO(id) {
+//     const db = await getDB()
+//     const collection = db.collection("cart")
+//     const result = await collection.find({ customerId: id }).toArray();
+//     return result;
+// }
+
+async function getCartDAO(id) {
     const db = await getDB()
-    const collection = db.collection("orders")
-    const result = await collection.insertOne(data);
+    const collection = db.collection("cart")
+    const result = await collection.aggregate([
+        {
+            $lookup: {
+                from: "products",
+                localField: 'productId',
+                foreignField: '_id',
+                as: 'productDetails'
+            }
+        },
+        {
+            $unwind: '$productDetails'
+        },
+        {
+            $project: {
+                _id: 1,
+                productId: 1,
+                productDetails: 1
+            }
+        }]).toArray();
     return result;
 }
 
-async function cancelOrderDAO(orderId){
-const db = await getDB()
-const collection = db.collection("orders")
-const result = await collection.updateOne({_id: ObjectId.createFromHexString(orderId)},{$set: {status: 'cancel'}})
-return result;
+async function saveToCartDAO(data) {
+    const db = await getDB()
+    const collection = db.collection("cart")
+    // const cartItems = await collection.find({productId: data?.productId}).toArray();
+    const cartItems = await collection.find({ productId: ObjectId.createFromHexString(data?.productId), uid:data?.uid }).toArray();
+    if (cartItems?.length == 0) {
+        const result = await collection.insertOne({ ...data, productId: ObjectId.createFromHexString(data.productId) })
+        const count = await collection.countDocuments({ uid: data?.uid })
+        return { ...result, count };
+    } else {
+        return {
+            message: "Already Added to the Cart"
+        }
+    }
+
 }
 
-async function getCartDAO(id){
-const db = await getDB()
-const collection = db.collection("cart")
-const result = await collection.find({customerId: id}).toArray();
-return result;
+async function deleteCartDAO(productId, uid) {
+    const db = await getDB()
+    const collection = db.collection("cart")
+    const result = await collection.deleteOne({ productId: ObjectId.createFromHexString(productId), uid });
+    if (result?.deletedCount > 0) {
+        const count = await collection.countDocuments({ uid })
+        result.count = count;
+    }
+    return result;
 }
 
-async function saveToCartDAO(data){
-const db = await getDB()
-const collection = db.collection("cart")
-const result = await collection.insertOne(data)
-return result;
+async function addressListDAO() {
+    const db = await getDB()
+    const collection = db.collection("address")
+    const result = await collection.find().toArray();
+    return result;
 }
 
-async function deleteCartDAO(orderId){
-const db = await getDB()
-const collection = db.collection("cart")
-const result = await collection.deleteOne({_id: ObjectId.createFromHexString(orderId)}, {$set: {status: 'Cancel'}})
-return result;
+async function saveAddressDAO(data) {
+    const db = await getDB()
+    const collection = db.collection("address")
+    const result = await collection.insertOne(data)
+    return result;
+}
+
+async function getCustomerByIdDAO(id) {
+    const db = await getDB()
+    const collection = db.collection("products")
+    const result = await collection.find({ _id: ObjectId.createFromHexString(id) }).toArray();
+    return result;
+}
+
+async function updateProfileDAO(id, data ) {
+    const db = await getDB()
+    const collection = db.collection("products")
+    const result = await collection.updateOne({ _id: ObjectId.createFromHexString(id) }, {$set: data})
+    return result;
 }
 
 module.exports = {
     regDAO,
     loginDAO,
-    getOrdersDAO, 
+    getOrdersDAO,
     saveOrderDAO,
     cancelOrderDAO,
     getProductsDAO,
     getProductByIdDAO,
     getCartDAO,
     saveToCartDAO,
-    deleteCartDAO
+    deleteCartDAO,
+    saveAddressDAO,
+    addressListDAO,
+    getCustomerByIdDAO,
+    updateProfileDAO
 }
